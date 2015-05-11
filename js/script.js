33aefb242ec07e5f6d2739a661ac70f7d79d4004
Catherine Roberts <Catherine@Catherines-MacBook-Pro.local>
2015-05-11 15:39:34 -0400

    syntax error in js, need help!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-$.ajax({
+$(document).ready(function(){
+
+
+	$.ajax({
    			dataType: "json",
   			url: "https://data.cityofnewyork.us/resource/xx67-kt59.json",
   			success:function(data){
-  			console.log(data);
+  				console.log(data);
+  				var arr = restaurantArray(data);
+  				var sortArr = sortedArr(arr);
+  				putOnPage(sortArr);
+  				runSearch();
 
-  			$.each(data, function(index,item){
 
-  					$('#restaurants').append("<h2>"+item.dba+'</h2><p>'+item.grade_date+'</p><p>'+item.violation_description+'</p><p>'+item.building+' '+item.street+' '+item.zipcode+'</p>');
-  				});
+  			// $.each(data, function(index,item){
+
+  			// 		$('#restaurants').append("<h2>"+item.dba+'</h2><p>'+item.grade_date+'</p><p>'+item.violation_description+'</p><p>'+item.building+' '+item.street+' '+item.zipcode+'</p>');
+  			// 	});
   			}
 		});
+
+	function restaurantArray(data){
+
+		var rest = [];
+
+		_.each(data.result.records,fuction(r,index){
+
+			var restaurant = {
+				name: r.dba,
+				date: r.grade_date,
+				address: r.building+' '+r.street+' '+r.zipcode
+			};
+
+			rest.push(restaurant);
+
+		});
+		return rest;
+	}
+})	
